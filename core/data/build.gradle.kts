51e7b05b0d5d622412c99a832d45b7cc8b5e2bd1
AlirezaNezami96 <alireza.nezami75@gmail.com>
2025-08-27 12:43:20 +0330

    add NoteRepository and implementation for managing notes and reminders

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation(project(":core:model"))
     implementation(project(":core:database"))
     implementation(project(":core:domain"))
+    implementation(project(":core:common"))
     implementation(libs.kotlinx.coroutines.android)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
