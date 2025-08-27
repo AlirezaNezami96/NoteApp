cce54b7bcc7fb1f91d90596e75dffe05ea32e897
AlirezaNezami96 <alireza.nezami75@gmail.com>
2025-08-27 13:25:43 +0330

    add design system components including themes, colors, and typography

---
+++ core/designsystem/src/main/java/alireza/nezami/designsystem/theme/Color.kt
@@ -0,0 +1,17 @@
+package alireza.nezami.designsystem.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Gray = Color(0xFFEFF0F4)
+val Gray200 = Color(0x80000000)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+val Blue = Color(0xFF3384F9)
+val Blue200 = Color(0x3384F940)
+
+val DarkGray900 = Color(0xFF121212)
+val DarkGray800 = Color(0xFF1E1E1E)
+val DarkGray700 = Color(0xFF2A2A2A)
+val Gray100 = Color(0xFFE0E0E0)
+val Blue300 = Color(0xFF4DA3FF)
+
